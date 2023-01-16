accfa805e18528a0271069ad4fa4a386afeb3a03
lekshmikjayan <lekshmikjayan27@gmail.com>
2023-01-16 22:55:02 +0530

    Revising Implicit wait

--- impWait.py
+++ impWait.py
@@ -3,6 +3,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
 service_obj = Service(r'C:\Users\leksh\Downloads\chromedriver_win32\chromedriver.exe');
 driver = webdriver.Chrome(service=service_obj)
 driver.implicitly_wait(5)
@@ -17,10 +20,24 @@ results = driver.find_elements(By.XPATH, "//div[@class='products']/div")
 #assert count > 0
 for result in results:
     result.find_element(By.XPATH,"div/button").click()
-time.sleep(5)
+
 driver.find_element(By.CSS_SELECTOR,"img[alt] = 'Cart'").click()
 driver.find_element(By.XPATH,"//button[text() = 'PROCEED TO CHECKOUT']").click()
-time.sleep(8)
-driver.find_element(By.CSS_SELECTOR, ".promoCode").send_keys("rahulshettyacademy")
-driver.find_element(By.CSS_SELECTOR, ".promobtn").click()
-time.sleep(9)
+
+#sum
+prices = driver.find_elements(By.CSS_SELECTOR, "tr td:nth-child(5) p")
+sum = 0
+for price in prices:
+    sum = sum + int(price.text)
+
+print(sum)
+totalAmount = int(driver.find_element(By.CSS_SELECTOR,".totAmt").text)
+assert sum == totalAmount
+driver.find_element(By.CSS_SELECTOR, ".promoCode").send_keys("lekshmikjayan")
+driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
+
+wait = WebDriverWait(driver,10)
+wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR,".promoInfo")))
+print(driver.find_element(By.CLASS_NAME,"promoInfo").text)
+
+#implicit wait is global ie, applicable to whole code, while explicit code is only for specified code
