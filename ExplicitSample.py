8bcbfc16dce59e3441e4863c8822e9e43c89fb67
lekshmikjayan <lekshmikjayan27@gmail.com>
2023-01-16 22:50:18 +0530

    Explicit Wait

---
+++ ExplicitSample.py
@@ -0,0 +1,52 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+expectedList = ['Beans =1 Kg','Banana -1 Kg', 'Mango - 1 Kg', 'Orange = 1 Kg', 'Pomegranate = 1 Kg' ]
+actualList = []
+service_obj = Service(r'C:\Users\leksh\Downloads\chromedriver_win32\chromedriver.exe');
+driver = webdriver.Chrome(service=service_obj)
+driver.implicitly_wait(2)
+
+
+driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
+driver.find_element(By.CSS_SELECTOR, ".search-keyword").send_keys("an")
+time.sleep(5)
+results = driver.find_elements(By.XPATH, "//div[@class='products']/div")
+count = len(results)
+
+assert count > 0
+for result in results:
+    actualList.append(result.find_element(By.XPATH, "h4").text)
+    result.find_element(By.XPATH,"div/button").click()
+
+time.sleep(5)
+#assert expectedList == actualList
+
+driver.find_element(By.CSS_SELECTOR,"img[alt = 'Cart']").click()
+driver.find_element(By.XPATH,"//button[text() = 'PROCEED TO CHECKOUT']").click()
+#time.sleep(5)
+#sum
+prices = driver.find_elements(By.XPATH, "//tr//td[5]//p")
+sum = 0
+for price in prices:
+    sum = sum + int(price.text)
+
+print(sum)
+totalAmount = int(driver.find_element(By.CSS_SELECTOR,".totAmt").text)
+assert sum == totalAmount
+
+driver.find_element(By.CSS_SELECTOR, ".promoCode").send_keys("rahulshettyacademy")
+driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
+
+wait = WebDriverWait(driver,10)
+wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR,".promoInfo")))
+print(driver.find_element(By.CLASS_NAME,"promoInfo").text)
+
+discountedAmount = float(driver.find_element(By.CSS_SELECTOR, ".discountAmt").text)
+#assert totalAmount > discountedAmount
+#implicit wait is global ie, applicable to whole code, while explicit code is only for specified code
