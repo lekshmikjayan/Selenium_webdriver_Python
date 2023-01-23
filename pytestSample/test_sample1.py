8e8366c3434a1f7794d8e5f097412623bfa29331
lekshmikjayan <lekshmikjayan27@gmail.com>
2023-01-23 16:22:32 +0530

    Test printing output

---
+++ pytestSample/test_sample1.py
@@ -0,0 +1,8 @@
+#pytest file also shud start or end with test_
+# function name shud start with test_
+
+def test_prog1():
+    print("Hw r u..?")
+
+def test_Sample2():
+    print("wie gehts es dir..?")
